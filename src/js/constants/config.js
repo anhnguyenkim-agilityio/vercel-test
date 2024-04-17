5c11c21996c3dc100ac0dde9b9281d4c2ed80a21
Anh Nguyen Kim <anh.nguyenkim@asnet.com.vn>
2024-04-17 14:55:59 +0700

    Delete src directory

--- src/js/constants/config.js
+++
@@ -1,7 +0,0 @@
-const api = {
-  URL_API: "http://localhost:3000",
-  END_POINT_PRODUCT: "product",
-  END_POINT_CART:"cart"
-};
-
-export { api };
--- src/js/helpers/selector.js
+++
@@ -1,4 +0,0 @@
-// Get element(s) by CSS selector:
-const querySelector = (selector) => document.querySelector(selector);
-
-export { querySelector };
