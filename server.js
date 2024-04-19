9fce790afdc17a97c5c20e3828684c18b1e89266
Anh Nguyen Kim <anh.nguyenkim@asnet.com.vn>
2024-04-19 11:21:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const fs = require('fs')
+
+const filePath = path.join("/tmp", "db.json");
+const data = {
+  "product": [
+    {
+      "name": "Big Potatos",
+      "price": 14.99,
+      "imgURL": "https://github.com/ThanhNgan110/javascript-training/assets/94752353/f9abd041-860e-46ce-b6c2-a11543da8cba",
+      "amount": 1,
+      "productId": "1",
+      "id": "de35"
+    }]}
+fs.writeFileSync(filePath, JSON.stringify(data));
+
+
 const server = jsonServer.create();
 
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
-const router = jsonServer.router(db);
-// const router = jsonServer.router("db/db.json");
+//const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+//const router = jsonServer.router(db);
+// const router = jsonServer.router("/tmp/db.json");
 
 const middlewares = jsonServer.defaults();
 
